6b6644be2dee435e0a58085e1d2e3c7a29188555
Cong <80631141+luucong1007@users.noreply.github.com>
2022-08-12 21:48:54 +0700

    first commit

---
+++ app/src/main/java/vn/lvc/quanlychitieu/database/DatabaseHelper.java
@@ -0,0 +1,41 @@
+package vn.lvc.quanlychitieu.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import vn.lvc.quanlychitieu.dao.ChiTieuDAO;
+import vn.lvc.quanlychitieu.dao.LoaiChiDAO;
+import vn.lvc.quanlychitieu.dao.LoaiThuDAO;
+import vn.lvc.quanlychitieu.dao.ThuNhapDAO;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, "qltc.db", null, 1);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        db.execSQL("PRAGMA foreign_keys = ON");
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(LoaiThuDAO.SQL_LOAI_THU);
+        sqLiteDatabase.execSQL(ThuNhapDAO.SQL_THU_NHAP);
+        sqLiteDatabase.execSQL(LoaiChiDAO.SQL_LOAI_CHI);
+        sqLiteDatabase.execSQL(ChiTieuDAO.SQL_CHI_TIEU);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("Drop table if exists " + LoaiThuDAO.TABLE_NAME);
+        sqLiteDatabase.execSQL("Drop table if exists " + ThuNhapDAO.TABLE_NAME);
+        sqLiteDatabase.execSQL("Drop table if exists " + LoaiChiDAO.TABLE_NAME);
+        sqLiteDatabase.execSQL("Drop table if exists " + ChiTieuDAO.TABLE_NAME);
+
+    }
+}
